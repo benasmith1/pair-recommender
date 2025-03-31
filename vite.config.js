8225e7eacfa1842ac43a4f9a80c5e2e9a5fc99ea
benasmith1 <81334348+benasmith1@users.noreply.github.com>
2025-03-31 14:19:41 -0600

    port 8080 json stuff

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,18 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: '0.0.0.0', // Vite will bind to all network interfaces
+    port: 8080, // Specify the port you want Vite to run on
     proxy: {
       '/submit': {
-        target: 'http://159.223.168.125:8080', //'http://localhost:5001', 
+        target: 'http://159.223.168.125:8080', // Adjust the API URL if needed
         changeOrigin: true,
         secure: false,
       },
-      host: '0.0.0.0', // Ensure Vite binds to all network interfaces
-      port: 8080, // Ensure the correct port
-    }
+    },
   },
-})
+});
