a648d4b7d214a09476fe2eaf6ac16d0483797b63
benasmith1 <81334348+benasmith1@users.noreply.github.com>
2025-03-31 23:35:47 -0600

    proxy gone

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,30 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   server: {
+//     host: '0.0.0.0', // Vite will bind to all network interfaces
+//     port: 8080, // Specify the port you want Vite to run on
+//     proxy: {
+//       '/submit': {
+//         target: 'https://pair-recommender-service-6oqt6.ondigitalocean.app', 
+//         changeOrigin: true,
+//         secure: false,
+//       },
+    
+//     },
+//   },
+//   preview: {
+//     allowedHosts: ['pair-recommender-client-6rb88.ondigitalocean.app'],
+//   },
+// });
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: '0.0.0.0', // Vite will bind to all network interfaces
-    port: 8080, // Specify the port you want Vite to run on
-    proxy: {
-      '/submit': {
-        target: 'https://pair-recommender-service-6oqt6.ondigitalocean.app', 
-        changeOrigin: true,
-        secure: false,
-      },
-    
-    },
+    host: '0.0.0.0',
+    port: 8080,
   },
   preview: {
     allowedHosts: ['pair-recommender-client-6rb88.ondigitalocean.app'],
